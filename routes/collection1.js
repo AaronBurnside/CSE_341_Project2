e8382f0be1707394b519f512cf130c013fd9ec15
AaronBurnside <aburns29@byui.edu>
2023-10-07 17:40:25 -0600

    a

--- routes/collection1.js
+++ routes/collection1.js
@@ -1,15 +1,20 @@
 const express = require('express');
 const router = express.Router();
 const C1Controller = require('../controller/C1');
-const validation = require('../middleware/validate');
+const { isAuthenticated } = require('../middleware/authenticate');
+//const validation = require('../middleware/validate');
 
 router.get('/', C1Controller.getAll);
 router.get('/:id', C1Controller.getSingle);
 
 
-router.post('/', validation.saveItem, C1Controller.createItem);
-router.put('/:id',validation.saveItem, C1Controller.updateItem);
-router.delete('/:id', C1Controller.deleteItem);
+// router.post('/', validation.saveItem, C1Controller.createItem);
+// router.put('/:id',validation.saveItem, C1Controller.updateItem);
+// router.delete('/:id', C1Controller.deleteItem);
 
+
+router.post('/', isAuthenticated, C1Controller.createItem);
+router.put('/:id',isAuthenticated, C1Controller.updateItem);
+router.delete('/:id',isAuthenticated, C1Controller.deleteItem);
 
 module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,18 @@
 const router = require('express').Router();
-
+const passport = require('passport');
 router.use('/', require('./swagger'));
 
-router.get('/', (req, res) => {res.send("Hello World");});
+//router.get('/', (req, res) => {res.send("Hello World");});
 
 router.use('/collection1', require('./collection1'));
+
+router.get('/login', passport.authenticate('github'), (req, res) => {});
+
+router.get('/logout', function(req, res, next){
+    req.logout(function(err){
+        if (err){return next(err);}
+        res.redirect('/');
+    });
+});
+
 module.exports = router;
